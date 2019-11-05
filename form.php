37818f60802a137611c17ad743465b0805fae4f8
Ayse Akdede <ayseazize.akdede@gmail.com>
2019-11-05 16:36:44 +0100

    ajout du mail validation et du mail envoie

--- form.php
+++ form.php
@@ -3,6 +3,8 @@
 // define variables and set to empty values
 $firstnameErr = $lastnameErr = $emailErr = $genderErr = $messageErr= "";
 $firstname = $lastname = $email = $gender = $message = "";
+$country = $_POST["country"];
+$subject = $_POST["subject"];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["firstname"])) {
@@ -44,4 +46,66 @@ function test_input($data) {
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
+}
+
+
+/* Namespace alias. */
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+
+/* Create a new PHPMailer object. Passing TRUE to the constructor enables exceptions. */
+$mail = new PHPMailer(TRUE);
+
+/* Open the try/catch block. */
+try {
+
+    /* Set the mail sender. */
+    $mail->setFrom('hackers@poulette.com', 'Hackers Poulette');
+
+    /* Add a recipient. */
+    $mail->addAddress($email, $name);
+
+    /* Set the subject. */
+    $mail->Subject = "Thank you for contacting us!";
+
+    /* Enable HTML */
+    $mail->isHTML(TRUE);
+
+    $mail->AddEmbeddedImage("assets/img/logo.png", "logo", "assets/img/logo.png");
+
+    /* Set the mail message body. */
+    $mail->Body =
+        "Message send automatically.<br>
+        Here is the informations you send us :<br>
+          <strong>Gender : </strong> $gender<br>
+          <strong>Firstname :</strong> $firstname<br>
+          <strong>Lastname :</strong> $lastname<br>
+          <strong>Mail :</strong> $email<br>
+          <strong>Country :</strong> $country<br>
+          <strong>Subject :</strong> $subject<br>
+          <strong>Message :</strong> $message<br>";
+
+    /* SMTP parameters. */
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->Username = 'zartovmaroteov@gmail.com';
+    $mail->Password = 'under087';
+
+    /* Finally send the mail. */
+    $mail->send();
+} catch (Exception $e) {
+    /* PHPMailer exception. */
+    echo $e->errorMessage();
+} catch (\Exception $e) {
+    /* PHP exception (note the backslash to select the global namespace Exception class). */
+    echo $e->getMessage();
 }
--- index.php
+++ index.php
@@ -10,57 +10,82 @@
 </head>
 <body>
 
-<div class ="top-container">
-  <h1> Contact Us </h1>
-  <img src="assets/img/logo.png" alt="The Logo">
-</div> 
+  <header class ="top-container">
+    <h1> Contact Us </h1>
+    <img src="assets/img/logo.png" alt="The Logo">
+  </header> 
 
-<div class="container">
-  <!-- FORM START -->
-  <form action="index.php" method="POST">
-    <!-- Gender -->
-    <div class="red"><?php echo $genderErr; ?></div>
-    <label for="gender">Gender</br></label>
-    <input type="radio" name="gender" value="female" <?php if ($gender == "female") echo 'checked=checked' ?>>Female
-    <input type="radio" name="gender" value="male" <?php if ($gender == "male") echo 'checked=checked' ?>>Male
-    <input type="radio" name="gender" value="other" <?php if ($gender == "other") echo 'checked=checked' ?>>Other</br></br>  
-    <!-- First name -->
-    <div class="red"><?php echo $firstnameErr; ?></div>
-    <label for="fname">First Name</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your firstname.." value="<?php echo test_input($firstname); ?>">
-    <!-- Last name -->
-    <div class="red"><?php echo $lastnameErr; ?></div>
-    <label for="lname">Last Name</label>
-    <input type="text" id="lname" name="lastname" placeholder="Your lastname.." value="<?php echo test_input($lastname); ?>">
-    <!-- Mail -->
-    <div class="red"><?php echo $emailErr; ?></div>
-    <label for="email">Email Address</label>
-    <input  type="text" name="email" maxlength="80" size="30" placeholder="example@hotmail.com" value="<?php echo test_input($email); ?>">
-    <!-- country -->
-    <label for="country">Country</label>
-    <select id="country" name="country" value="<?php echo test_input($country); ?>">
-      <option value="australia">Australia</option>
-      <option value="canada">Canada</option>
-      <option value="usa">USA</option>
-    </select>
-    <!-- Subject -->
-    <label for="subject">Subject</label> 
-    <select id="subject" name="subject">
-      <option value="others" <?php echo (isset($_POST['subject']) && $_POST['subject'] == 'others') ? 'selected="selected"' : ''; ?>>Others</option>
-      <option value="rasberrypi"  <?php echo (isset($_POST['subject']) && $_POST['subject'] == 'rasberrypi') ? 'selected="selected"' : ''; ?>>Rasberry Pi</option>
-      <option value="accessories"  <?php echo (isset($_POST['subject']) && $_POST['subject'] == 'accessories') ? 'selected="selected"' : ''; ?>>Accessories </option>
-    </select>
-    <!-- message area -->
-    <div class="red"><?php echo $message; ?></div>
-    <label for="message">Message</label>
-    <textarea id="message" name="message" placeholder="Type your message here ..." style="height:100px"><?php echo test_input($message); ?></textarea>
-    <!-- submit button -->
-    <input type="submit" name="submit" value="Submit">
-  </form>
+  <div class="container">
+    <!-- FORM START -->
+    <form action="index.php" method="POST">
+      <!-- Gender -->
+      <div class="red"><?php echo $genderErr; ?></div>
+      <label for="gender">Gender<br></label>
+      <input type="radio" name="gender" value="female" <?php if ($gender == "female") echo 'checked=checked' ?>>Female
+      <input type="radio" name="gender" value="male" <?php if ($gender == "male") echo 'checked=checked' ?>>Male
+      <input type="radio" name="gender" value="other" <?php if ($gender == "other") echo 'checked=checked' ?>>Other<br><br>
+      <!-- First name -->
+      <div class="red"><?php echo $firstnameErr; ?></div>
+      <label for="fname">First Name</label>
+      <input type="text" id="fname" name="firstname" placeholder="Your firstname.." value="<?php echo test_input($firstname); ?>">
+      <!-- Last name -->
+      <div class="red"><?php echo $lastnameErr; ?></div>
+      <label for="lname">Last Name</label>
+      <input type="text" id="lname" name="lastname" placeholder="Your lastname.." value="<?php echo test_input($lastname); ?>">
+      <!-- Mail -->
+      <div class="red"><?php echo $emailErr; ?></div>
+      <label for="email">Email Address</label>
+      <input  type="text" name="email" maxlength="80" size="30" placeholder="example@hotmail.com" value="<?php echo test_input($email); ?>">
+      <!-- country -->
+      <label for="country">Country</label>
+      <select id="country" name="country" value="<?php echo test_input($country); ?>">
+      
+        <option>-- Choose your country</option>
+        <option value="Austria">Austria</option>
+        <option value="Belgium">Belgium</option>
+        <option value="Bulgaria">Bulgaria</option>
+        <option value="Croatia">Croatia</option>
+        <option value="Cyprus">Cyprus</option>
+        <option value="Czech_Republic">Czech Republic</option>
+        <option value="Denmark">Denmark</option>
+        <option value="Estonia">Estonia</option>
+        <option value="Finland">Finland</option>
+        <option value="France">France</option>
+        <option value="Germany">Germany</option>
+        <option value="Greece">Greece</option>
+        <option value="Hungary">Hungary</option>
+        <option value="Ireland">Ireland</option>
+        <option value="Italy">Italy</option>
+        <option value="Latvia">Latvia</option>
+        <option value="Lithuania">Lithuania</option>
+        <option value="Luxembourg">Luxembourg</option>
+        <option value="Malta">Malta</option>
+        <option value="Netherlands">Netherlands</option>
+        <option value="Poland">Poland</option>
+        <option value="Portugal">Portugal</option>
+        <option value="Romania">Romania</option>
+        <option value="Slovak_Republic">Slovak Republic</option>
+        <option value="Slovenia">Slovenia</option>
+        <option value="Spain">Spain</option>
+        <option value="Sweden">Sweden</option>
+        <option value="United_Kingdom">United Kingdom</option>
+      </select>
+      <!-- Subject -->
+      <label for="subject">Subject</label> 
+      <select id="subject" name="subject">
+        <option value="others" <?php echo (isset($_POST['subject']) && $_POST['subject'] == 'others') ? 'selected="selected"' : ''; ?>>Others</option>
+        <option value="rasberrypi"  <?php echo (isset($_POST['subject']) && $_POST['subject'] == 'rasberrypi') ? 'selected="selected"' : ''; ?>>Rasberry Pi</option>
+        <option value="accessories"  <?php echo (isset($_POST['subject']) && $_POST['subject'] == 'accessories') ? 'selected="selected"' : ''; ?>>Accessories </option>
+      </select>
+      <!-- message area -->
+      <div class="red"><?php echo $messageErr; ?></div>
+      <label for="message">Message</label>
+      <textarea id="message" name="message" placeholder="Type your message here ..." style="height:100px"><?php echo test_input($message); ?></textarea>
+      <!-- submit button -->
+      <input type="submit" name="submit" value="Submit">
+    </form>
+  
+  </div>
  
-</div>
-
-
-    
 </body>
 </html>
